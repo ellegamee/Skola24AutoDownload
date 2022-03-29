ba03254b283ba5e770be0ed9ac1e6dbcd1ee5b07
Elias Axonov <ed.axonov@gmail.com>
2022-03-29 15:23:59 +0200

    non working gecko

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from webdriver_manager.firefox import GeckoDriverManager
 
 def main():
-    driver = webdriver.Firefox()
+    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     driver.get("www.svt.se")
 
 if __name__ == "__main__":
