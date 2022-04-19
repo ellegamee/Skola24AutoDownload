8dc109ba750177d034f97431afb3f52ab1bb329c
Elias Axonov <ed.axonov@gmail.com>
2022-04-19 14:56:28 +0200

    Initial selenium test
    I got chromedriver installed to the projekt running and opening one website. This shows promise for the future.

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 
 def main():
-    driver = webdriver.Chrome()
-    driver.get("www.svt.se")
+    driver = webdriver.Chrome('./chromedriver.exe')
+    driver.get("https://web.skola24.se/timetable/timetable-viewer/norrkoping.skola24.se/Ebersteinska%20gymnasiet/")
 
 if __name__ == "__main__":
     main()
