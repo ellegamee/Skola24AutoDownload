ff91a28c2f1f4240bf5a844b05a836b4d06efbee
eberElev <ed.axonov@gmail.com>
2022-04-26 12:56:10 +0200

    Wait presance of schoo list
    Selenium waits now for school list button and opens it. This will make it possible for everyschool in the disctrict to use the program and change which school they want their scheman from.

--- __main__.py
+++ __main__.py
@@ -1,8 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+import time
 
 def main():
     driver = webdriver.Chrome('./chromedriver.exe')
     driver.get("https://web.skola24.se/timetable/timetable-viewer/norrkoping.skola24.se/Ebersteinska%20gymnasiet/")
+    
+    schoolList = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div[2]/div/div[2]/div[1]/div[1]/div[1]/div/div/button")))
+    schoolList.click()
+    time.sleep(5)
+
+    driver.quit()
 
 if __name__ == "__main__":
     main()
